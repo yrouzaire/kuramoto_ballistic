d4a8fa716c42dc1f232ce2beb7de61876d13c459
yrouzaire <32304770+yrouzaire@users.noreply.github.com>
2023-01-29 12:12:13 +0100

    Update two_defects.jl

--- exploration/two_defects.jl
+++ exploration/two_defects.jl
@@ -1,9 +1,9 @@
 cd("D:/Documents/Research/projects/kuramoto_ballistic")
-using JLD2, StatsBase, Distributions, LinearAlgebra, Parameters, Random, BenchmarkTools, Hungarian
+using JLD2, StatsBase, Distributions, LinearAlgebra, Parameters, Random, BenchmarkTools, Hungarian, LambertW
 include("../methods.jl")
 const global R0 = 1
 using Plots, ColorSchemes, LaTeXStrings
-gr(box=true, fontfamily="sans-serif", label=nothing, palette=ColorSchemes.tab10.colors[1:10], grid=false, markerstrokewidth=0, linewidth=1.3, size=(400, 400), thickness_scaling=1.5);
+pyplot(box=true, fontfamily="sans-serif", label=nothing, palette=ColorSchemes.tab10.colors[1:10], grid=false, markerstrokewidth=0, linewidth=1.3, size=(400, 400), thickness_scaling=1.5);
 plot();
 cols = cgrad([:black, :blue, :green, :orange, :red, :black]);
 plot()
@@ -25,10 +25,10 @@ T = 0.1 # temperature for angle diffusion
 v0 = 0.25
 sigma = 0.0
 every = 2;
-r0s = 5:5:15
+r0s = 5:5:25
 dt = determine_dt(T, sigma, v0, N, rho)
 every = 2;
-tmax = 40;
+tmax = 400;
 times = every:every:tmax;
 R = 50
 
@@ -41,8 +41,8 @@ z = @elapsed for i in each(r0s)
         t = 0
         dft = DefectTracker(pos, thetas, N, L, L, t)
         # try 
-            dft, pos, thetas, t = track!(dft, pos, thetas, omegas, psis, T, v0, N, L, L, dt, t, times)
-            dfts[i, r] = dft
+        dft, pos, thetas, t = track!(dft, pos, thetas, omegas, psis, T, v0, N, L, L, dt, t, times)
+        dfts[i, r] = dft
         # catch e
         #     println("Error at r0 = $(r0) and r = $(r)")
         #     println(e)
@@ -94,10 +94,8 @@ for i in each(r0s)
     rt = Rts_reverse_avg[i]
     plot!(rt[2:end], label="r0 = $(r0s[i])", m=true)
 end
-p
 # savefig("figures/two_defects/Rt_several_r0s_rho$(round(rho,digits=2))_v0$(v0)_sigma$(sigma)_tmax$(tmax)_R$(R)_N$(Ntarget)..png")
-
+p
+plot!(x -> 3x^0.33, c=:black, line=:dot)
+plot!(x -> exp(lambertw(2π / 2 * x) / 2), c=:black, line=:dash)
 ## B. Study MSD with two defects far apart
-
-
-
