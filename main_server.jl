02ac2b9612be950447c0198d980aafdb78afac13
yrouzaire <32304770+yrouzaire@users.noreply.github.com>
2023-03-25 18:15:21 +0100

    Update main_server.jl

--- main_server.jl
+++ main_server.jl
@@ -3,12 +3,12 @@ include("IDrealisation.jl");
 using JLD2, LinearAlgebra, Statistics, Hungarian
 include("methods.jl");
 
-## ---------------- Nature of the Phase Transition ---------------- ##
-comments = "The goal of this script is to pass through the transition line, 
-in both direction (keeping σ or v0 constant) and to compute correlation functions.
-Here for T = 0.1 and ρ = 1."
+## ---------------- Tracking a pair of defects ---------------- ##
+comments = "From the defects data, one will be able to infer : \n
+A. the separating distance between the two defects R(t) \n
+B. the MSD and diffusion coeff of an individual defect. "
 # Physical Params 
-Ntarget = Int(1E3)
+Ntarget = Int(1E4)
 aspect_ratio = 1
 T = 0.1
 R0 = 1
@@ -17,52 +17,103 @@ rhoc = 4.51 / π
 
 # Initialisation parameters
 init_pos = "random"
-init_theta = "hightemp"
+init_theta = "pair"
 r0 = 20.0
 q = 1.0
-params_init = Dict(:init_pos => init_pos, :init_theta => init_theta, :r0 => r0, :q => q)
+params_init = Dict(:init_pos => init_pos, :init_theta => init_theta, :r0 => NaN, :q => q)
 
 # Simulation parameters
-v0sigs = vcat([(0.25,sigm) for sigm in 0:0.025:0.2],[(v,0.1) for v in logspace(0.03,1,10)])
+v0sigs = [(0.5,0),(0.5,0.1)]
+r0s = 5:10:35
 tmax = 1E2
-# times = collect(0:tmax/30:tmax) # linear time
-times = logspace(1,tmax,30,digits=1) # log time
+times = 0:5:tmax # linear time
 
-P = zeros(length(v0sigs), length(times))
-C = Array{Vector{Float64}}(undef, length(v0sigs), length(times))
-xi = zeros(length(v0sigs), length(times))
-n = zeros(length(v0sigs), length(times))
-
-z = @elapsed for i in each(v0sigs)
+z = @elapsed for i in each(v0sigs), j in each(r0s)
     v0, sigma = v0sigs[i]
-    println("v0 = $v0, σ = $sigma, $(100i/length(v0sigs))%")
+    r0 = r0s[j]
+
+    println("v0 = $v0, σ = $sigma, r0 = $r0 ,  $(100i/length(v0sigs))%")
     N, Lx, Ly = effective_number_particle(Ntarget, rho, aspect_ratio)
     dt = determine_dt(T, sigma, v0, N, rho)
 
+    params_init = Dict(:init_pos => init_pos, :init_theta => init_theta, :r0 => r0, :q => q)
+    
     param = Dict(:Ntarget => Ntarget, :aspect_ratio => aspect_ratio,
         :rho => rho, :T => T, :R0 => R0, :sigma => sigma, :v0 => v0,
         :N => N, :Lx => Lx, :Ly => Ly, :params_init => params_init)
 
-    system = System(param)
-
     t = 0.0
-    token = 1
-
-    for tt in eachindex(times)
-        evolve(system, times[tt]) # evolves the systems up to times[tt]
-        
-        P[i,tt]  = polarOP(system)[1]
-        corr_tmp    = corr(system)
-        C[i,tt]  = corr_tmp
-        xi[i,tt] = corr_length(corr_tmp)
-        n[i,tt]  = number_defects(system)
-    end
-
+    system = System(param)
+    dft = DefectTracker(system, t)
+    dft, pos, thetas, t = track!(dft,system,times)
 end
 prinz(z)
 
-filename = "data/nature_phase_transition_r$real.jld2"
-JLD2.@save filename Ntarget v0sigs rho params_init T P C n xi aspect_ratio times tmax comments rhoc runtime = z
+filename = "data/DFT_pair_r$real.jld2"
+# JLD2.@save filename Ntarget v0sigs rho params_init T P C n xi aspect_ratio times tmax comments rhoc runtime = z
+
+
+
+# ## ---------------- Nature of the Phase Transition ---------------- ##
+# comments = "The goal of this script is to pass through the transition line, 
+# in both direction (keeping σ or v0 constant) and to compute correlation functions.
+# Here for T = 0.1 and ρ = 1."
+# # Physical Params 
+# Ntarget = Int(1E3)
+# aspect_ratio = 1
+# T = 0.1
+# R0 = 1
+# rho = 1
+# rhoc = 4.51 / π
+
+# # Initialisation parameters
+# init_pos = "random"
+# init_theta = "hightemp"
+# r0 = 20.0
+# q = 1.0
+# params_init = Dict(:init_pos => init_pos, :init_theta => init_theta, :r0 => r0, :q => q)
+
+# # Simulation parameters
+# v0sigs = vcat([(0.25,sigm) for sigm in 0:0.025:0.2],[(v,0.1) for v in logspace(0.03,1,10)])
+# tmax = 1E2
+# # times = collect(0:tmax/30:tmax) # linear time
+# times = logspace(1,tmax,30,digits=1) # log time
+
+# P = zeros(length(v0sigs), length(times))
+# C = Array{Vector{Float64}}(undef, length(v0sigs), length(times))
+# xi = zeros(length(v0sigs), length(times))
+# n = zeros(length(v0sigs), length(times))
+
+# z = @elapsed for i in each(v0sigs)
+#     v0, sigma = v0sigs[i]
+#     println("v0 = $v0, σ = $sigma, $(100i/length(v0sigs))%")
+#     N, Lx, Ly = effective_number_particle(Ntarget, rho, aspect_ratio)
+#     dt = determine_dt(T, sigma, v0, N, rho)
+
+#     param = Dict(:Ntarget => Ntarget, :aspect_ratio => aspect_ratio,
+#         :rho => rho, :T => T, :R0 => R0, :sigma => sigma, :v0 => v0,
+#         :N => N, :Lx => Lx, :Ly => Ly, :params_init => params_init)
+
+#     system = System(param)
+
+#     t = 0.0
+#     token = 1
+
+#     for tt in eachindex(times)
+#         evolve(system, times[tt]) # evolves the systems up to times[tt]
+        
+#         P[i,tt]  = polarOP(system)[1]
+#         corr_tmp    = corr(system)
+#         C[i,tt]  = corr_tmp
+#         xi[i,tt] = corr_length(corr_tmp)
+#         n[i,tt]  = number_defects(system)
+#     end
+
+# end
+# prinz(z)
+
+# filename = "data/nature_phase_transition_r$real.jld2"
+# JLD2.@save filename Ntarget v0sigs rho params_init T P C n xi aspect_ratio times tmax comments rhoc runtime = z
 
 
 # ## ---------------- No Hysteresis and Nature Phases ---------------- ##
