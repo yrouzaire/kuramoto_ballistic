f90b6fd014ac31a50405ec70012cff2dc2d82e2a
yrouzaire <32304770+yrouzaire@users.noreply.github.com>
2023-05-30 10:07:19 +0200

    Update movies.jl

--- exploration/movies.jl
+++ exploration/movies.jl
@@ -28,23 +28,47 @@ include("../parameters.jl")
 system = System(param)
 get_pos(system)
 evolve!(system, 10)
-evolve!(system, 100)
-plot_thetas(system,particles=true,nb_neighbours=true,defects=false,vertical=true,size=(512,512))
+evolve!(system, 30)
+# plot_thetas(system,particles=true,nb_neighbours=true,defects=false,vertical=true,size=(512,512))
 number_defects(system)
-plot_thetas(system,particles=true,nb_neighbours=true,defects=true,vertical=true,size=(512,512))
-## --------------- Movies --------------- ##
+plot_thetas(system,particles=true,nb_neighbours=false,defects=true,vertical=true,size=(512,512))
+
+## --------------- A single movie --------------- ##
+include("../parameters.jl")
+tmax = Int(100)
+times = collect(0:2:tmax) # linear time
+
+params_init = Dict(:init_pos => init_pos, :init_theta => init_theta, :r0 => r0, :q => q)
+params_phonons = Dict(:phonons => phonons, :phonon_amplitude => phonon_amplitude, :phonon_k => phonon_k, :phonon_omega => phonon_omega)
+param = Dict(:Ntarget => Ntarget, :aspect_ratio => aspect_ratio,
+    :rho => rho, :T => T, :R0 => R0, :sigma => sigma, :v0 => v0,
+    :N => N, :Lx => Lx, :Ly => Ly, :params_init => params_init, 
+    :params_phonons => params_phonons)
+
+z = @elapsed anim = movies(param, times, 
+        particles=true,
+        defects=true,
+        nb_neighbours=false,
+        verbose=true)
+
+prinz(z) 
+filepath = "films/divers/test.mp4"
+mp4(anim, filepath, fps=30)
+
+
+
+
+## --------------- Scanning on parameters --------------- ##
 include("../parameters.jl")
 inits_pos = ["square","random","RSA","PDS"]
 # inits_pos = ["square","tri"]
-# inits_pos = ["square"]
+inits_pos = ["random"]
 
 inits_theta = ["hightemp","pair"]
-inits_theta = ["hightemp"]
+inits_theta = ["pair"]
 
-omegas = collect(0.02)
-
-tmax = Int(500)
-times = collect(0:2:tmax) # linear time
+tmax = Int(1000)
+times = collect(0:1:tmax) # linear time
 # times = logspace(1,tmax,10) # log time
 anims = Array{Any}(undef,length(inits_pos),length(inits_theta))
 n = 0
@@ -63,9 +87,9 @@ z = @elapsed for i in each(inits_pos), j in each(inits_theta) , k in each(omegas
 
     try 
         anim = movies(param, times, 
-                particles=false,
-                defects=false,
-                nb_neighbours=true,
+                particles=true,
+                defects=true,
+                nb_neighbours=false,
                 verbose=true)
 
         anims[i,j] = anim
@@ -73,7 +97,8 @@ z = @elapsed for i in each(inits_pos), j in each(inits_theta) , k in each(omegas
         if phonons 
             filename *= "_2Dphonons_A$(phonon_amplitude)_k$(round(Int,phonon_k/(2π/Lx)))_ω$(round(phonon_omega,digits=2))"
         end
-        filepath = "impact_init/films/phonons/$(init_theta)/"*filename*".mp4"
+        filepath = "films/$(init_theta)/"*filename*".mp4"
+        # filepath = "impact_init/films/phonons/$(init_theta)/"*filename*".mp4"
         mp4(anim, filepath, fps=30)
     catch e 
         println(e)
