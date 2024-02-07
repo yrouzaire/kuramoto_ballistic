d60a9a1eb3a4d4a75165d708944c05882500b6fb
Ylann Rouzaire <32304770+yrouzaire@users.noreply.github.com>
2024-02-07 11:40:41 +0100

    Update FSS_determiner_transition.jl

--- exploration/FSS_determiner_transition.jl
+++ exploration/FSS_determiner_transition.jl
@@ -16,6 +16,7 @@ filename = "data/FSS_to_determine_transition.jld2"
 hrun(runtimes)
 Ntargets
 v0sigs
+v0s = [v for (v,x) in v0sigs]
 Ps_avg = nanmean(Ps, 4)[:,:,:,1]
 ns_avg = nanmean(ns, 4)[:,:,:,1]
 xis_avg = nanmean(xis, 4)[:,:,:,1]
@@ -36,9 +37,22 @@ for i in 1:length(v0sigs), j in each(Ntargets), k in 1:length(times)
     Cs_avg[i, j, k] = mean([Cs[i, j, k, r] for r in indices])
 end
 
-## ---------------- Plotting ---------------- ##
+# # ---------------- Plotting ---------------- ##
 p=plot(axis=:log, legend=false, xlabel=L"N", ylabel=L"P")
 for i in each(v0sigs)
     plot!(Ntargets, Ps_avg[i, :, 30], label="v0sig = $(v0sigs[i])", rib=0, m=true)
+end
+p
+
+
+## ---------------- Investigating the time saturation of the magnetisation ---------------- ##
+## ---------------- Investigating the time saturation of the magnetisation ---------------- ##
+## ---------------- Investigating the time saturation of the magnetisation ---------------- ##
+## ---------------- Investigating the time saturation of the magnetisation ---------------- ##
+p=plot(axis=:log, legend=false, xlabel=L"t", ylabel=L"P")
+for i in 1#each(v0sigs)
+    for j in each(Ntargets)
+        plot!(times, Ps_avg[i, j, :], label="v0sig = $(v0sigs[i])", rib=0, m=true)
+    end
 end
 p
